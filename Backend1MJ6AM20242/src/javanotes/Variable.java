659a96a5852f90cbfb8f7cc77c210f693f709ef6
Bernardo A Mira García <bamira1@misena.edu.co>
2024-08-15 07:25:18 -0500

    clase 15 de agosto
    Repaso variables

---
+++ Backend1MJ6AM20242/src/javanotes/Variable.java
@@ -0,0 +1,37 @@
+package javanotes;
+
+public class Variable {
+
+
+    public static void main(String[] args) {
+
+
+        byte moveLeft; // declarando la variable
+
+        moveLeft = 127;
+
+        short moveLeft2 = (short) moveLeft;
+
+        System.out.println(moveLeft2);
+
+        moveLeft2 = 130;
+
+        // recibe dos valores numericos pero uno de ellos es de tipo string
+
+        int num1 = 19;
+
+        String num2 = "23";
+
+        //parseo de Variable
+
+        int newNum2= Integer.parseInt(num2);
+        
+        float promedio = (num1 + newNum2)/ 2;
+
+        System.out.println(promedio);
+
+
+
+
+    }
+}
